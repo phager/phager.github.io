1882eed9029dafe3ef118d3474872540197df50d
phager <paul@tarohealth.com>
2025-07-27 16:00:37 -0700

    Add lint and remove end-of-line whitespace

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import reactRecommended from "eslint-plugin-react/configs/recommended.js";
+
+export default tseslint.config(
+  {
+    ignores: ["dist/**", "node_modules/**", ".husky/**"],
+  },
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+    ...reactRecommended,
+    languageOptions: {
+      ...reactRecommended.languageOptions,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2021,
+      },
+    },
+    rules: {
+        'no-trailing-spaces': 'error',
+    }
+  }
+);
--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,5 @@ export default defineConfig({
       polyfill: true
     }
   },
-  
-}) 
+
+})
